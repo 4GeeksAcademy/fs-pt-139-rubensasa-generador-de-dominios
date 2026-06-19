8d9f94f6db50d7a956610226dea4603010e3c689
rubensasa <rebensasa@gmail.com>
2026-06-19 19:05:06 +0000

    corrigiendo partes

--- src/app.js
+++ src/app.js
@@ -5,26 +5,23 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-let i,j,k,l;
-
-
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 
   let pronombre = ['Sr', 'El','nuestro'];
   let adj = ['bueno', 'grande','gran','tonto','hermoso','idiota'];
-  let nombre = ['gato', 'perro','girafa','hombre',];
-  let extension =['.com','.net','.net','.es','.us','.eu','.io'];
+  let nombre = ['gato', 'perro','girafa','hombre','sensei'];
+  let extension =['.com','.edu','.net','.es','.us','.eu','.io'];
   
-  for (i=0;i<pronombre.length;i++)
+  for (let i=0;i<pronombre.length;i++)
   {
-    for (j=0;j<adj.length;j++)
+    for (let j=0;j<adj.length;j++)
     {
-      for (k=0;k<nombre.length;k++)
+      for (let k=0;k<nombre.length;k++)
       {
-        for (l=0;l<extension.length;l++)
-        {console.log(pronombre[i]+adj[j]+nombre[k]+extension[l]);}
+        for (let l=0;l<extension.length;l++)
+        {console.log(pronombre[i] + adj[j] + nombre[k] + extension[l]);}
       }
     }
   }
